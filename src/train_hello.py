113f6513cfc8cdadf31d4a85d4efdcb30f743c16
VINCENT GIBSON <vrg7bj@virginia.edu>
2025-09-18 15:15:54 -0400

    Revert "chore: initial project scaffold"
    This reverts commit 034517095322895ed049d4f7b8e2216688ea5be5.

--- src/train_hello.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-
-X, y = load_iris(return_X_y=True)
-Xtr, Xte, ytr, yte = train_test_split(X, y, test_size=0.2, random_state=42)
-model = LogisticRegression(max_iter=1000).fit(Xtr, ytr)
-print("Accuracy:", accuracy_score(yte, model.predict(Xte)))
